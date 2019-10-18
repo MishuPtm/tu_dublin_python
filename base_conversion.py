8afb7b600085bea46b0a307ea33d98f838d456dc
mishuptm <ionutserban.petrescu@gmail.com>
2019-10-18 21:17:40 +0100

    Adjusted format to match the online file

--- base_conversion.py
+++ base_conversion.py
@@ -1,7 +1,8 @@
 """
 Author: Ionut Petrescu
 Date: 26.09.2019
-Converting from base 10 to any base between 2 and 16
+
+Updated to allow user to select origin base and output base
 """
 DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 
@@ -48,13 +49,13 @@ def prompt_input_number(lower_limit=0, upper_limit=99999999999999999):
 def select_base(desc):
     base_int = -1
     while base_int < 0:
-        print(f"Insert a base {desc} in the range of 2 and 16 \n")
+        print(f"Insert a base {desc} in the range of 2 and 16 ")
         base_int = prompt_input_number(2, 16)
     return base_int
 
 
 def insert_number_in_base(base):
-    digits = DIGITS[0:base]  # this is to check for valid input
+    digits = DIGITS[0:base]  # this makes sure we use only digits in this base
     valid_input = False
     number_str = ""
 
@@ -74,11 +75,12 @@ def convert_from_to(input_base, output_base, input_value):
         return input_value
 
     if not input_base == 10:
-        input_value = convert_to_decimal(input_base, str(input_value))
+        input_value = convert_to_decimal(input_base, input_value)
 
     return convert_from_decimal(output_base, int(input_value))
 
 
+# Call this method to prompt user for input
 def convert_from_to_ui():
     from_base = select_base("to convert from")
     nb_to_convert = insert_number_in_base(from_base)
@@ -86,5 +88,7 @@ def convert_from_to_ui():
     result = convert_from_to(from_base, to_base, nb_to_convert)
     print(f"{nb_to_convert} in base {from_base} is {result} in base {to_base}")
 
+
+#print(convert_to_decimal(2, "010100101000"))
 
 convert_from_to_ui()
